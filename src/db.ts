0cd2f5f66182b0809a5ba19d7063a15d21502960
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2023-10-18 19:20:00 -0400

    Package.json Exports & db bug fix

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 if (!process.env.DATABASE_URL) throw new Error('No DATABASE_URL env var');
 
-const db = new PrismaClient();
+export const db = new PrismaClient();
 await db.$connect();
 export default db;
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export * from './definitions.js';
 export * as discord from './discord/index.js';
+export * from './db.js';
+export * from './definitions.js';
 export * from './prices.js';
 export * from './validation.js';
