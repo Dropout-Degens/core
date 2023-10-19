0b8e0afa46d9a4b6ce2e62ecc879b61785d88e95
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2023-10-19 07:15:03 -0400

    Only Refresh Token If Needed

--- src/discord/refresh-token.ts
+++ src/discord/refresh-token.ts
@@ -3,10 +3,11 @@ import db from "../db.js";
 import { Routes, RESTPostOAuth2RefreshTokenURLEncodedData, RESTPostOAuth2RefreshTokenResult } from 'discord.js';
 import { botREST } from "./REST.js";
 
-
 export async function refreshToken(user: Pick<users, 'snowflake'|'discord_access_token'|'discord_access_expiry'|'discord_refresh_token'>, force = false) {
     if (!process.env.DISCORD_CLIENT_ID) throw new Error('Environment variable `DISCORD_CLIENT_ID` not set!');
     if (!process.env.DISCORD_CLIENT_SECRET) throw new Error('Environment variable `DISCORD_CLIENT_SECRET` not set!');
+
+    if (!force && user.discord_access_expiry + 60000n > Date.now()) return;
 
     const tokenResponse = await botREST.post(Routes.oauth2TokenExchange(), {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded', },
