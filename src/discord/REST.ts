ff114638251a446ec8b9afc1f672c15f4c69cad2
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2023-11-04 11:31:45 -0400

    Mock Bot REST for build mode because Next.js doesn't like me

--- src/discord/REST.ts
+++ src/discord/REST.ts
@@ -1,6 +1,22 @@
 import { REST as DiscordREST } from '@discordjs/rest';
 
-export const botREST = new DiscordREST({authPrefix: 'Bot'});
-botREST.setToken(process.env.DISCORD_BOT_TOKEN!);
+export const botREST = (()=> {
+    if (!process.env.DISCORD_BOT_TOKEN) {
+        const mockBotREST = new DiscordREST({authPrefix: 'Bot'});
+        console.warn('Discord bot token not found. Creating a mock REST client which will warn when called.');
+        for (const method of ['delete', 'get', 'patch', 'post', 'put'] as const)
+            mockBotREST[method] = () => {
+                console.warn(`Discord bot REST method ${method.toUpperCase()} called without token. This can be safely ignored in build mode.`);
+                return Promise.resolve();
+            };
+
+        return mockBotREST;
+    }
+
+    const botREST = new DiscordREST({authPrefix: 'Bot'});
+    botREST.setToken(process.env.DISCORD_BOT_TOKEN!);
+    console.log('botRESTToken', process.env.DISCORD_BOT_TOKEN);
+    return botREST;
+})();
 
 export const bearerTokenREST = new DiscordREST({authPrefix: 'Bearer'});
