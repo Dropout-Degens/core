191bef4a4ca4188704642b38418691df57317282
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2023-12-16 09:14:53 -0500

    Start Logging Stripped-Down DB Queries

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from '@prisma/client'
 
 if (!process.env.DATABASE_URL) throw new Error('No DATABASE_URL env var');
 
-export const db = new PrismaClient();
+export const db = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+    errorFormat: 'pretty',
+});
 await db.$connect();
 export default db;
 
