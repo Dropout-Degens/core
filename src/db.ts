8821d9958194ebfec246071c13cbf438a1c25a2d
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2024-03-25 19:38:56 -0400

    Improve `db.ts` for Next.js dev servers

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client';
+
+if (typeof window !== 'undefined') throw new Error('Cannot connect to the database from the client!');
 
 if (!process.env.DATABASE_URL) throw new Error('No DATABASE_URL env var');
 
-export const db = new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'],
-    errorFormat: 'pretty',
-});
-await db.$connect();
-export default db;
+declare global {
+    var db____internal: undefined | ReturnType<typeof newPrismaClientBase> | Awaited<ReturnType<typeof newPrismaClientBase>>;
+}
 
-process.on('exit', () => db.$disconnect());
+function newPrismaClient() {
+    if (globalThis.db____internal !== undefined) return globalThis.db____internal;
+    return globalThis.db____internal = newPrismaClientBase();
+}
+
+function newPrismaClientBase() {
+    const db = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+        errorFormat: 'pretty',
+    })
+    .$extends({
+        name: 'Community Masterlist Custom Search Extension',
+    });
+    ;
+
+    db.$connect();
+    process.on('exit', () => db.$disconnect());
+
+    return db;
+}
+
+export const db = newPrismaClient();
+export default db;
