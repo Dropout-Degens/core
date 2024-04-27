79b3554f33ebe14021f1daf913542426b9c6bdfb
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2024-04-26 22:27:06 -0400

    Add updated plan IDs for All Access

--- src/prices.ts
+++ src/prices.ts
@@ -2,19 +2,19 @@ import { BillingPeriod, PurchasablePlan } from "./definitions.js"
 
 export const StripePrices: SubscriptionPriceMap = {
     [PurchasablePlan.AllAccess]: {
-        [BillingPeriod.year]: 'price_1NpHNvKuVSp9UKA3Cx3NRd9q',
-        [BillingPeriod.months3]: 'price_1P22XyKuVSp9UKA3LHVU6czR',
-        [BillingPeriod.months1]: 'price_1P22HNKuVSp9UKA3gTswgUcP',
-        [BillingPeriod.weekly]: 'price_1NNNNcKuVSp9UKA3ernITrTU',
+        [BillingPeriod.year]: ['price_1NpHNvKuVSp9UKA3Cx3NRd9q'],
+        [BillingPeriod.months3]: ['price_1P22XyKuVSp9UKA3LHVU6czR'],
+        [BillingPeriod.months1]: ['price_1P22HNKuVSp9UKA3gTswgUcP'],
+        [BillingPeriod.weekly]: ['price_1NNNNcKuVSp9UKA3ernITrTU'],
     }
 } as const satisfies SubscriptionPriceMap;
 
 export const WhopPrices: SubscriptionPriceMap = {
     [PurchasablePlan.AllAccess]: {
-        [BillingPeriod.year]: 'plan_dJMpw98NBBnvy',
-        [BillingPeriod.months3]: 'plan_MoNgrgc2tYhWM',
-        [BillingPeriod.months1]: 'plan_81MKG7LrO2MlU',
-        [BillingPeriod.weekly]: 'plan_TadvdbFxNT0zm',
+        [BillingPeriod.year]: ['plan_dJMpw98NBBnvy', 'plan_GAEa8emHiWSJO'],
+        [BillingPeriod.months3]: ['plan_MoNgrgc2tYhWM', 'plan_jcHLswLL8mWU6'],
+        [BillingPeriod.months1]: ['plan_81MKG7LrO2MlU', 'plan_L97kXB1BheOJC'],
+        [BillingPeriod.weekly]: ['plan_TadvdbFxNT0zm', 'plan_4GJOw4rCOX5pv'],
         other: [
             'plan_OyBfhHoCKnrCi', // 2-month
             'plan_PmCmLoGkyYQGh', // free
@@ -22,7 +22,7 @@ export const WhopPrices: SubscriptionPriceMap = {
     },
 
     [PurchasablePlan.Van_HighRoller]: {
-        [BillingPeriod.year]: 'plan_rzFf2KqDXIqiZ',
+        [BillingPeriod.year]: ['plan_rzFf2KqDXIqiZ'],
     },
 
     ignored: [
@@ -30,6 +30,6 @@ export const WhopPrices: SubscriptionPriceMap = {
     ],
 } as const satisfies SubscriptionPriceMap;
 
-export type PricePeriods =  Readonly<Partial<{ other: readonly string[] } & Record<BillingPeriod, string>>>;
+export type PricePeriods =  Readonly<Partial<Record<BillingPeriod|'other', string[]>>>;
 
 export type SubscriptionPriceMap = Readonly<Partial<{ ignored?: readonly string[] } & Record<Exclude<PurchasablePlan, PurchasablePlan.Any>, PricePeriods>>>
