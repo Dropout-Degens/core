6c1ee515a807b8847ae3491d22ebe8ba33717f30
BellCube Dev <33764825+BellCubeDev@users.noreply.github.com>
2024-08-01 12:49:15 -0400

    Remove Useless Stuff

--- src/definitions.ts
+++ src/definitions.ts
@@ -102,35 +102,6 @@ export enum RoleFlags {
 
 export default RoleFlags;
 
-export enum KarmaPerMessageByRole {
-    _any        = 5,
-    AnyPremiumRole  = 10,
-    Van_HighRoller  = 25,
-    AnyStaffRole    = 10,
-}
-
-export enum KarmaPerMessageDailyCapByRole {
-    _any        = 500,
-    AnyPremiumRole  = 1250,
-    Van_HighRoller  = 2500,
-    AnyStaffRole    = -1,
-}
-
-export enum KarmaPerReactionByRole {
-    _any = 5,
-}
-
-export enum KarmaPerReactionDailyCapByRole {
-    _any = 25,
-}
-
-export enum KarmaDailyBonusByRole {
-    _any = 0,
-    AnyPremiumRole = 250,
-    Van_HighRoller = 500,
-    AnyStaffRole = 0,
-}
-
 export enum PurchasablePlan {
     Van_HighRoller = RoleFlags.Van_HighRoller,
     AllAccess = RoleFlags.AllAccessRaw,
@@ -171,51 +142,3 @@ export type Coupon<TGenerated extends boolean = false, TUsesStripe extends boole
 
 export type PlanFreeDays = Partial<Record<DiscountSource, number>>;
 export type FreeDays = Partial<Record<PurchasablePlan, PlanFreeDays>>;
-
-
-
-
-/** Represents a single coupon */
-//export interface CouponDefinition {
-//    /** Number multiplied against the price to determine how much should be waived (e.g. a 75% discount would be 0.75) */
-//    amount: number;
-//
-//    /** The type of subscription this coupon is applicable to */
-//    planType: Exclude<PurchasablePlan, PurchasablePlan.Any>;
-//
-//    /** The billing period this coupon should apply to */
-//    billingPeriod: BillingPeriod;
-//
-//    /** How many billing periods this coupon lasts for */
-//    duration: number;
-//
-//    /** The promo code string used for Whop
-//     *
-//     * Schema:
-//     * ${Snowflake}-${PseudoRandomFillTo40Chars}-${BillingPeriod}-${Duration}-${Amount}
-//    */
-//    promoCode?: string;
-//
-//    /** Where the coupon was obtained from */
-//    source: DiscountSource;
-//}
-
-//export interface CouponGenerated extends CouponDefinition {
-//    promoCode: string;
-//}
-
-//export type Coupon<TGenerated extends boolean = false> = TGenerated extends true ? CouponGenerated : CouponDefinition;
-
-
-/** A list of discounts applicable to a given subscription type */
-//export interface Discounts {
-//    /** How many days the user has for free in this tier
-//     *
-//     * Organized by the source of the discount
-//    */
-//    free: Partial<Record<DiscountSource, number>>;
-//    /** A list of coupons for this tier in this customer's arsenal */
-//    coupons: Coupon<true>[];
-//}
-
-//export type UsersDiscount = Partial<Record<PurchasablePlan, Discounts>>;
