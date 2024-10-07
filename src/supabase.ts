2c889058db2955493307b53da8dcd3093544ab19
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2024-10-07 11:05:10 -0400

    typo fix

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.SUPABASE_URL
 if (!supabaseUrl) throw new Error('SUPABASE_URL is not defined in the environment variables.')
 const supabaseSecretKey = process.env.SUPABASE_SECRET_KEY
-if (!supabaseSecretKey) throw new Error('SUPABSE_SECRET_KEY is not defined in the environment variables.')
+if (!supabaseSecretKey) throw new Error('SUPABASE_SECRET_KEY is not defined in the environment variables.')
 
 export const supabase = createClient(
     supabaseUrl,
