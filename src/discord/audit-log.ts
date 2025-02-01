24eb79bd59e9bf4046cb97aba007c00b4476ebbd
BellCube <33764825+BellCubeDev@users.noreply.github.com>
2025-02-01 14:57:48 -0500

    Allow audit logging of Message context menu interactions

--- src/discord/audit-log.ts
+++ src/discord/audit-log.ts
@@ -1,5 +1,5 @@
-import type { ChatInputCommandInteraction, RawFile, WebhookMessageCreateOptions } from "discord.js";
-import { Routes } from 'discord-api-types/v10';
+import type { ChatInputCommandInteraction, InteractionType, MessageInteractionMetadata, RawFile, WebhookMessageCreateOptions } from "discord.js";
+import { ApplicationCommandType, Routes } from 'discord-api-types/v10';
 import { botREST } from "./REST";
 
 
@@ -16,25 +16,46 @@ const AuditLogChannelIDs = DISCORD_AUDIT_LOG_CHANNELS!.trim().split(",").map((c)
 //    })
 //}
 
-export async function auditAdminCommand(execution: ChatInputCommandInteraction, def: InteractionDefinition<ApplicationCommandSubCommandData, true>) {
-    const subcommandGroup = execution.options.getSubcommandGroup();
-    const subcommand = execution.options.getSubcommand();
+export async function auditAdminCommand(execution: MessageContextMenuCommandInteraction | ChatInputCommandInteraction, def: InteractionDefinition<MessageApplicationCommandData | ApplicationCommandSubCommandData, true>) {
+    const subcommandGroup = execution.isChatInputCommand() ? execution.options.getSubcommandGroup() : null;
+    const subcommand = execution.isChatInputCommand() ? execution.options.getSubcommand() : null;
 
-    return await auditRaw({
-        content: `${execution.user.toString()} executed \`/${execution.commandName + (subcommandGroup ? ' ' + subcommandGroup : '') + (subcommand ? ' ' + subcommand : '')}\``,
-        embeds: [{
-            title: def.interaction.description,
-            fields: def.interaction.options?.map(option => {
-                const value = execution.options.get(option.name);
-                return {
-                    name: value !== null ? `✔   ${option.name}` : `✖   ~~*${option.name}*~~`,
-                    value: `${option.description}\n` + (value === null ? '*Unspecified*' : value.attachment ? `[Attachment](${value.attachment.url})` : value.channel?.toString() ?? value.member?.toString() ?? value.message?.toString() ?? value.role?.toString() ?? value.user?.toString() ?? value.value?.toString() ?? '```json\n' + value + '\n```'),
-                    inline: false,
-                }
-            }),
-        }],
-        allowedMentions: { parse: [] },
-    });
+    if (def.interaction.type === ApplicationCommandType.Message) {
+        const messageExecution = execution as MessageContextMenuCommandInteraction;
+        return await auditRaw({
+            content: `${messageExecution.user.toString()} executed \`/${messageExecution.commandName}\``,
+            embeds: [{
+                title: messageExecution.commandName,
+                fields: [
+                    {name: 'Message Author', value: messageExecution.targetMessage.author.toString(), inline: true},
+                    {name: 'Message', value: `https://discord.com/channels/${messageExecution.guildId}/${messageExecution.channelId}/${messageExecution.targetMessage.id})`, inline: true},
+                    {
+                        name: 'Message Content',
+                        value: messageExecution.targetMessage.content,
+                        inline: false,
+                    }
+                ]
+            }],
+            allowedMentions: { roles: [] },
+        });
+    } else {
+        const slashCommandExecution = execution as ChatInputCommandInteraction;
+        return await auditRaw({
+            content: `${slashCommandExecution.user.toString()} executed \`/${slashCommandExecution.commandName + (subcommandGroup ? ' ' + subcommandGroup : '') + (subcommand ? ' ' + subcommand : '')}\``,
+            embeds: [{
+                title: def.interaction.description,
+                fields: def.interaction.options?.map(option => {
+                    const value = slashCommandExecution.options.get(option.name);
+                    return {
+                        name: value !== null ? `✔   ${option.name}` : `✖   ~~*${option.name}*~~`,
+                        value: `${option.description}\n` + (value === null ? '*Unspecified*' : value.attachment ? `[Attachment](${value.attachment.url})` : value.channel?.toString() ?? value.member?.toString() ?? value.message?.toString() ?? value.role?.toString() ?? value.user?.toString() ?? value.value?.toString() ?? '```json\n' + value + '\n```'),
+                        inline: false,
+                    }
+                }),
+            }],
+            allowedMentions: { roles: [] },
+        });
+    }
 }
 
 export async function auditRaw(body: WebhookMessageCreateOptions, ...attachments: RawFile[]) {
